a8fe382b707cf2715ffe2f220cf381d03eb0ca3b
Samrat Roy <samratcodebase@gmail.com>
2025-09-20 20:21:17 +0530

    Added User Model

---
+++ src/Models/User.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+
+const UserSchema = new mongoose.Schema({
+    user: {
+        type: String,       
+        required: true,     
+        unique: true   
+    }
+}, {
+    timestamps: true 
+});
+
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
