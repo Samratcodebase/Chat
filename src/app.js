e275f6b791dc8fb7bf34eeeef00342b7da6cc0c0
Samrat Roy <samratcodebase@gmail.com>
2025-09-22 22:06:04 +0530

    Feature/RateLimiter

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ import express from "express";
 import authRoutes from "./Routes/Auth.route.js";
 import errorHandler from "./Middlewares/ErrorHandler.js";
 import cookieParser from "cookie-parser";
+import limiter from "./Middlewares/RateLimit.js";
 const app = express();
 app.use(express.json());
 app.use(cookieParser())
+app.use(limiter);
 app.use("/api/auth", authRoutes);
 
 app.use(errorHandler);
